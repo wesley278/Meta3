931d42ae41eea33c56bfb86ce855bf238b991fb8
Wesley souza <wesleydasilva278@gmail.com>
2024-02-10 21:01:15 -0300

    Enviando Alteracao de avaliacao

--- src/Components/Home/Avaliacao/Avaliacao.jsx
+++ src/Components/Home/Avaliacao/Avaliacao.jsx
@@ -12,15 +12,15 @@ function Avaliacao() {
     <div className={style.BoxFundoAvaliacao}> 
     <div className={style.BoxAvaliacao}><div className={style.DivIcon} ><ImQuotesLeft fontSize={50} /></div>
       <p>Tive o prazer de contar com os serviços da <span>Meta3</span> para a revitalização da fachada do meu estabelecimento comercial e estou extremamente satisfeito com o resultado. A equipe da Meta3 demonstrou profissionalismo desde o início, oferecendo uma consultoria personalizada para escolher os revestimentos mais adequados ao estilo do meu prédio.</p>
-      <h6>-Matheus</h6>
+      <h6>-Carlos</h6>
     </div>
     <div className={style.BoxAvaliacao}><div className={style.DivIcon} ><ImQuotesLeft fontSize={50} /></div>
     <p>Contratar a <span>Meta3</span> para o projeto de revestimento da fachada foi uma decisão acertada. Além da qualidade dos produtos, destaco o comprometimento da empresa em cumprir prazos. O cronograma foi seguido à risca, e a equipe demonstrou uma eficiência notável durante todo o processo. A transparência na comunicação e o cuidado com os detalhes foram pontos cruciais para o sucesso do projeto.</p> 
-    <h6>-Sergio</h6>
+    <h6>-Pedro</h6>
     </div>
     <div className={style.BoxAvaliacao}> <div className={style.DivIcon} ><ImQuotesLeft fontSize={50} /></div>
     <p>A <span>Meta3</span> superou minhas expectativas ao trabalhar na renovação da fachada do meu prédio. Destaco a abordagem inovadora da empresa, que trouxe opções de revestimentos especiais verdadeiramente únicas e personalizadas para atender às minhas preferências estéticas. A equipe de design foi atenciosa e criativa, incorporando elementos personalizados que tornaram a fachada do meu edifício uma verdadeira obra de arte.</p>
-    <h6>-Valdei</h6>
+    <h6>-Ricardo</h6>
     </div>
     </div> 
 
